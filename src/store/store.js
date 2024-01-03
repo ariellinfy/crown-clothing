1a1cdb3975d6421ccf0b652c448b7c6eca391828
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-01-02 23:15:40 -0700

    Incorporated redux-thunk for category reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,26 +2,14 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import { thunk } from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
-// const customLoggerMiddleware = (store) => (next) => (action) => {
-//   if (!action.type) {
-//     return next(action);
-//   }
-
-//   console.log("type: ", action.type);
-//   console.log("payload: ", action.payload);
-//   console.log("currentState: ", store.getState());
-
-//   next(action);
-
-//   console.log("next state: ", store.getState());
-// };
-
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
