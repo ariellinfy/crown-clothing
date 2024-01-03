74d1d10d89edf586c65c3037b1d509365995efeb
Ariel Lin <72408793+ariellinfy@users.noreply.github.com>
2024-01-02 22:31:41 -0700

    Incorporated useReducer()

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
@@ -6,13 +7,20 @@ import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 
+import { UserContext } from "./contexts/user.context";
+
 const App = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
+        <Route
+          path="auth"
+          element={currentUser ? <Home /> : <Authentication />}
+        />
         <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
